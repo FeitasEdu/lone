0cf005d0085837999ebb9b74bdb899effa494eb1
ASUS-PC\Asus <liu--qiong@hotmail.com>
2017-07-22 22:31:02 +0800

    no message

--- feitasacademy/models/sale.py
+++ feitasacademy/models/sale.py
@@ -7,5 +7,5 @@ from odoo import models,fields,api
 _logger = logging.getLogger(__name__)
 
 class AcademySaleOrder(models.Model):
-	_inherit = "sale.order"
-	test_char = fields.Char(string="测试字段Char")
+    _inherit = "sale.order"
+    test_char = fields.Char(string="测试字段Char")
